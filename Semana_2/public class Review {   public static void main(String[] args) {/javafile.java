1597fc11f9c4ac31ca2da663d0be6f2cedcdd73f
robdinovildib <109745594+robdinovildib@users.noreply.github.com>
2022-08-14 18:10:59 -0500

    Delete Semana_2/public class Review {   public static void main(String[] args) { directory

--- Semana_2/public class Review {   public static void main(String[] args) {/javafile.java
+++
@@ -1,7 +0,0 @@
-public class Review {
-  public static void main(String[] args) {
-//The main method executes the tasks of the class
-System.out.println("My first Java Program from scratch!");
-}
- 
-}
