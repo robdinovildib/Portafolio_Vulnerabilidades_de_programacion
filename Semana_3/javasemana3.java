6be562177b67b401c2363f45b6d40b903b8d79c1
robdinovildib <109745594+robdinovildib@users.noreply.github.com>
2022-08-14 21:07:32 -0500

    Create javasemana3.java

---
+++ Semana_3/javasemana3.java
@@ -0,0 +1,10 @@
+public class MyProfile {
+	public static void main(String[] args) {  
+    String name = "Roberto Alejandro Vilchis Meza"; 
+    int age = 18;
+    double desiredSalary = 100000.00;
+    char gender = 'm';
+    boolean lookingForJob = true;
+
+	}
+}
