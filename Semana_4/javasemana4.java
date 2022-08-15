e87ae597e15bba7abdad4ff4453c9c063f0b3930
robdinovildib <109745594+robdinovildib@users.noreply.github.com>
2022-08-14 23:11:05 -0500

    Create javasemana4.java

---
+++ Semana_4/javasemana4.java
@@ -0,0 +1,8 @@
+
+int number = 5;
+
+number += 3; // valor ahora es 8
+number -= 4; // valor ahora es 4
+number *= 6; // valor ahora es 24
+number /= 2; // valor ahora es 12
+number %= 7; // valor ahora es 5
