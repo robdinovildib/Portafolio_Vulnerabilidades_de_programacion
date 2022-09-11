1ff06a5e1057545c713f95e22ba6977276c3656a
robdinovildib <109745594+robdinovildib@users.noreply.github.com>
2022-09-11 17:56:55 -0500

    Create nested.java

---
+++ Semana_7/nested.java
@@ -0,0 +1,15 @@
+
+boolean studied = true;
+boolean wellRested = true;
+
+if (wellRested) {
+  System.out.println("Best of luck today!");  
+  if (studied) {
+    System.out.println("You are prepared for your exam!");
+  } else {
+    System.out.println("Study before your exam!");
+  }
+}
+
+// Prints: Best of luck today!
+// Prints: You are prepared fo
