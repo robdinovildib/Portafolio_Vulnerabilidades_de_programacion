5a392e14938f0601a750fa8dc726245d9bde5566
robdinovildib <109745594+robdinovildib@users.noreply.github.com>
2022-09-11 17:57:16 -0500

    Create andoperdores.java

---
+++ Semana_7/andoperdores.java
@@ -0,0 +1,4 @@
+System.out.println(true && true); // Prints: true
+System.out.println(true && false); // Prints: false
+System.out.println(false && true); // Prints: false
+System.out.println(false && false); // Prints: false
